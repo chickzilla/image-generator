b630c69569c9cf643abc0a38b6e26596860ddac4
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-06-07 15:48:00 +0700

    api: create generate controller

---
+++ api/src/generate/generate.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
+import { GenerateService } from './generate.service';
+import { GenerateRequestDTO } from './dto';
+
+@Controller('')
+export class GenerateController {
+  constructor(private readonly generateService: GenerateService) {}
+
+  @Post('/generate')
+  async generate(@Body() payload: GenerateRequestDTO) {
+    try {
+      return await this.generateService.generate(payload);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
